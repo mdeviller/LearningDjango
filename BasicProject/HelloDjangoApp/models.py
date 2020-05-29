65969c4eee15e41df295ecd4da2ae38b223c64cc
Devillers <marcdevillers@hotmail.com>
2020-05-30 00:27:32 +0200

    step 3

--- BasicProject/HelloDjangoApp/models.py
+++ BasicProject/HelloDjangoApp/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
-# Create your models here.
+
+from django.db import models
+from treebeard.mp_tree import MP_Node
+
+class Category(MP_Node):
+    name = models.CharField(max_length=30)
+
+    node_order_by = ['name']
+
+    def __unicode__(self):
+        return 'Category: %s' % self.name
